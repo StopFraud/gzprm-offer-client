f7c7db580f53416e2865ca6a0afe906aef1328d1
StopFraud <94336292+StopFraud@users.noreply.github.com>
2022-07-15 16:29:37 -0700

    Update gzprm-offer-client.py

--- gzprm-offer-client.py
+++ gzprm-offer-client.py
@@ -58,7 +58,7 @@ def service_check(pip):
 #            print (d1)
 
 #            sys.exit()
-            print ("{:<20} {:<43} {:<10} {}".format(Fore.WHITE+data["name"]+" "+data["surname"], Fore.YELLOW+data["email"],data["password"],Fore.GREEN+data["phrase"]+Fore.WHITE))
+            print ("{:<22} {:<44} {:<10} {}".format(Fore.WHITE+data["name"]+" "+data["surname"], Fore.YELLOW+data["email"],data["password"],Fore.GREEN+data["phrase"]+Fore.WHITE))
 #            time.sleep(1)
 
 
